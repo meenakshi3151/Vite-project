3049b8a4c08907daab62758b214d1cf7623d9d15
Meenakshi Gupta <125037000+meenakshi3151@users.noreply.github.com>
2025-04-18 08:36:48 +0530

    Update vite.config.js

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -4,4 +4,11 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+    server: {
+    // ... other server options
+    allowedHosts: true, // Allow all hosts
+    // OR
+    // allowedHosts: ['localhost', '127.0.0.1', 'yourdomain.com'] // Allow specific hosts
+  }
+
 })
